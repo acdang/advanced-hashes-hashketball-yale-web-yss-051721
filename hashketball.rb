cd513a15b1bd8668f10166939aa493086c03083b
ambitious-caller-8426 <anhcdang@gmail.com>
2021-05-25 21:47:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,7 +129,6 @@ end
 
 # Write code here
 def num_points_scored(player)
-  #knows the number of points scored by each player (FAILED - 1)
   result = []
   game_hash.find do |key, value|
     result = value[:players].find do |array_index|
