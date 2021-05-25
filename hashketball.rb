666b84ba60054abf9587d17939285109deb88469
ambitious-caller-8426 <anhcdang@gmail.com>
2021-05-25 21:36:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,6 +133,7 @@ def num_points_scored(player)
   result = game_hash.each do |key, value|
     value[:players].find do |array_index|
       value[:players][array_index][:player_name] == player
+      binding.pry
     end
   end
 end
